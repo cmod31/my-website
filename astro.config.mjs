c6130e8ca8971c42f1626e87678924befb7c0768
Yannik Ain <yannik@vaporvee.com>
2022-04-11 13:29:34 +0200

    added turbolinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 import nodejs from '@astrojs/node';
+
+import turbolinks from "@astrojs/turbolinks";
+
 // https://astro.build/config
 export default defineConfig({
-    adapter: nodejs()
+  adapter: nodejs(),
+  integrations: [turbolinks()]
 });
