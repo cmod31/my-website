d530ee126facf3bd23a143d9fe5f13d36221a65e
Yannik Ain <yannik@vaporvee.com>
2022-04-11 13:33:12 +0200

    added tailwind support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import nodejs from '@astrojs/node';
-
 import turbolinks from "@astrojs/turbolinks";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: nodejs(),
-  integrations: [turbolinks()]
+  integrations: [turbolinks(), tailwind()]
 });
