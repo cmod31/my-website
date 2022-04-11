19c371d1db57db829b9578fb9253578381277d0f
Yannik Ain <yannik@vaporvee.com>
2022-04-11 13:51:10 +0200

    fixed the whole thing no 404 lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import nodejs from '@astrojs/node';
 import turbolinks from "@astrojs/turbolinks";
 
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: nodejs(),
   integrations: [turbolinks()]
 });
