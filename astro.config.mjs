5773a552f5b16dd3b222599d09988853d4a73f2f
Yannik Ain <yannik@vaporvee.com>
2022-04-11 13:37:26 +0200

    deleted tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from 'astro/config';
 import nodejs from '@astrojs/node';
 import turbolinks from "@astrojs/turbolinks";
 
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: nodejs(),
-  integrations: [turbolinks(), tailwind()]
+  integrations: [turbolinks()]
 });
