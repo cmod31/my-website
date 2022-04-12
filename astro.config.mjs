417c209b80e80fbb3ccf440026225822e6d39639
Yannik Ain <yannik@vaporvee.com>
2022-04-12 17:12:32 +0200

    fixed stuff and added video bg(idk why)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import turbolinks from "@astrojs/turbolinks";
 
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [turbolinks()]
