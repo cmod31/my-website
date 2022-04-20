e5e726598a2bee11e271d7d343a723d262bfaaf3
Yannik Ain <yannik@vaporvee.com>
2022-04-20 16:55:11 +0200

    removed fork-awesome

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import turbolinks from "@astrojs/turbolinks";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
   integrations: [turbolinks()]
 });
